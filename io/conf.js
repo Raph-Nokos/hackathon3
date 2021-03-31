a1c3d1a06feba063a88be32390c1b976ea30cdd9
Joel-Jorland <joel.jorland@gmail.com>
2021-03-31 22:35:08 +0200

    modif 1

--- io/conf.js
+++ io/conf.js
@@ -1,44 +1,45 @@
-const socketio = require("socket.io");
+const socketio = require("socket.io")
 
 module.exports = function (server) {
-  // io server
-  const io = socketio(server);
+	// io server
+	const io = socketio(server)
 
-  // game state (players list)
-  const players = {};
+	// game state (players list)
+	const players = {}
 
-  io.on("connection", function (socket) {
-    // register new player
-    players[socket.id] = {
-      x: 0,
-      y: 0,
-      size: 30,
-      speed: 2,
-      c: "#" + (((1 << 24) * Math.random()) | 0).toString(16)
-    };
-    // PLAYERS MOOVES
-    socket.on("move left", function () {
-      players[socket.id].x -= players[socket.id].speed;
-    });
-    socket.on("move up", function () {
-      players[socket.id].y -= players[socket.id].speed;
-    });
-    socket.on("move right", function () {
-      players[socket.id].x += players[socket.id].speed;
-    });
-    socket.on("move down", function () {
-      players[socket.id].y += players[socket.id].speed;
-    });
-    // PLAYER ACTIONS
+	io.on("connection", function (socket) {
+		// register new player
+		players[socket.id] = {
+			x: 0,
+			y: 0,
+			size: 30,
+			speed: 2,
+			color: "#" + (((1 << 24) * Math.random()) | 0).toString(16),
+		}
+		console.log(players)
+		// PLAYERS MOOVES
+		socket.on("move left", function () {
+			players[socket.id].x -= players[socket.id].speed
+		})
+		socket.on("move up", function () {
+			players[socket.id].y -= players[socket.id].speed
+		})
+		socket.on("move right", function () {
+			players[socket.id].x += players[socket.id].speed
+		})
+		socket.on("move down", function () {
+			players[socket.id].y += players[socket.id].speed
+		})
+		// PLAYER ACTIONS
 
-    // delete disconnected player
-    socket.on("disconnect", function () {
-      delete players[socket.id];
-    });
-    function update() {
-      io.emit("players list", Object.values(players));
-    }
+		// delete disconnected player
+		socket.on("disconnect", function () {
+			delete players[socket.id]
+		})
+		function update() {
+			io.emit("players list", Object.values(players))
+		}
 
-    setInterval(update, 1000 / 60);
-  });
-};
+		setInterval(update, 1000 / 60)
+	})
+}
