927a0edf7bba16d48d1b1bff77fa9b0ec7389120
alexandresegault <alexandresegault@gmail.com>
2021-03-31 23:52:37 +0200

    new base, easier for socket

---
+++ io/conf.js
@@ -0,0 +1,67 @@
+const socketio = require("socket.io");
+
+module.exports = function (server) {
+  // io server
+  const io = socketio(server);
+
+  // game state (players list)
+  const players = {};
+
+  io.on("connection", function (socket) {
+    // register new player
+    players[socket.id] = {
+      x: 0,
+      y: 0,
+      size: 30,
+      speed: 2,
+      color: "#" + (((1 << 24) * Math.random()) | 0).toString(16)
+    };
+
+    // CHAT PART
+
+    // socket.emit("askName"); // on demande leurs noms aux joueurs
+
+    // socket.on("message", text => {
+    //   io.emit("message", text, players[socket.id]); // io.emit envoie a tous les clients
+    // });
+    // socket.on("name", text => {
+    //   players[socket.id].name = text;
+    //   console.log(players[sock.id]);
+    //   io.emit("message", "Bonjour " + text, players[socket.id]); // io.emit envoie a tous les clients
+    // });
+
+    // PLAYERS MOOVES
+    socket.on("move left", function () {
+      if (players[socket.id].x >= 2)
+        players[socket.id].x -= players[socket.id].speed;
+    });
+    socket.on("move up", function () {
+      if (players[socket.id].y >= 2)
+        players[socket.id].y -= players[socket.id].speed;
+    });
+    socket.on("move right", function () {
+      if (players[socket.id].x <= 910) {
+        players[socket.id].x += players[socket.id].speed;
+      }
+    });
+    socket.on("move down", function () {
+      if (players[socket.id].y <= 648)
+        players[socket.id].y += players[socket.id].speed;
+    });
+    socket.on("position", function () {
+      // console.log(`${players[socket.id].x}, ${players[socket.id].y}`);
+    });
+    // PLAYER ACTIONS
+
+    // delete disconnected player
+    socket.on("disconnect", function () {
+      console.log("user disconnected");
+      delete players[socket.id];
+    });
+    function update() {
+      io.emit("players list", Object.values(players));
+    }
+
+    setInterval(update, 1000 / 60);
+  });
+};
---
+++ public/game.js
@@ -0,0 +1,56 @@
+const socket = io();
+
+let players = [];
+
+const ctx = canvas.getContext("2d");
+
+function drawPlayers() {
+  players.forEach(function ({ x, y, size, color }) {
+    ctx.beginPath();
+    ctx.rect(x, y, size, size);
+    ctx.fillStyle = color;
+    ctx.fill();
+  });
+}
+socket.on("players list", function (list) {
+  players = list;
+});
+function update() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // 1.
+  movePlayer();
+  drawPlayers(); // 2.
+  requestAnimationFrame(update); // 3.
+}
+// first call
+requestAnimationFrame(update);
+// PLAYER MOOVES
+const keyboard = {};
+
+window.onkeydown = function (e) {
+  keyboard[e.keyCode] = true;
+};
+
+window.onkeyup = function (e) {
+  delete keyboard[e.keyCode];
+};
+
+function movePlayer() {
+  if (keyboard[37]) {
+    socket.emit("move left");
+    socket.emit("position");
+  }
+  if (keyboard[38]) {
+    socket.emit("move up");
+    socket.emit("position");
+  }
+  if (keyboard[39]) {
+    socket.emit("move right");
+    socket.emit("position");
+  }
+  if (keyboard[40]) {
+    socket.emit("move down");
+    socket.emit("position");
+  }
+}
+
+// PLAYER ACTIONS
