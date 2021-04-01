59db2704867e884dcea477d0369c749b9635cb90
Joel-Jorland <joel.jorland@gmail.com>
2021-04-01 18:22:03 +0200

    barre de vie

--- public/chat.js
+++ public/chat.js
@@ -1,64 +1,27 @@
-// const writeEvent = (text, player) => {
-//   // <ul> elem
-//   const parent = document.querySelector("#events");
-//   // <li> elem
-//   const el = document.createElement("li");
-//   el.innerHTML = `${player.name ? player.name : "incognito"}: ${text}`;
-//   parent.appendChild(el);
-//   parent.scrollTop = parent.scrollHeight;
-// };
+const writePlayers = (players) => {
+	const parent = document.querySelector("#listPlayers")
 
-const writePlayers = players => {
-  const parent = document.querySelector("#listPlayers");
+	players.forEach((pl) => {
+		const color = pl.color
 
-  players.forEach(pl => {
-    const color = pl.color;
-
-    if (!Array.from(parent.children).some(e => e.id === pl.color)) {
-      const el = document.createElement("li");
-      el.id = color;
-      el.innerText = `${pl.name}: ${pl.score} points`;
-      el.style.color = color;
-      parent.appendChild(el);
-    } else {
-      const el = document.getElementById(color);
-      el.innerText = `${pl.name} : ${pl.score} points`;
-    }
-  });
-};
+		if (!Array.from(parent.children).some((e) => e.id === pl.color)) {
+			const el = document.createElement("li")
+			el.id = color
+			el.innerText = `${pl.name}: ${pl.score} points`
+			el.style.color = color
+			parent.appendChild(el)
+		} else {
+			const el = document.getElementById(color)
+			pl.hp <= 0
+				? (el.innerText = `${pl.name} s'est fait plumé`)
+				: (el.innerText = `${pl.name} : ${pl.score} points`)
+		}
+	})
+}
 
 const askName = () => {
-  let name = prompt("whats your name ?");
-  socket.emit("name", name);
-};
+	let name = prompt("whats your name ?")
+	socket.emit("name", name)
+}
 
-socket.on("askName", askName);
-
-// const onFormSubmitted = e => {
-//   e.preventDefault();
-//   const input = document.querySelector("#chat");
-//   const text = input.value;
-//   input.value = "";
-//   socket.emit("message", text);
-// };
-
-// const onFormSubmittedName = e => {
-//   e.preventDefault();
-//   const input = document.querySelector("#userName");
-//   const text = input.value;
-//   input.value = "";
-//   socket.emit("name", text);
-//   document.getElementById("askname").style.display = "none";
-// };
-
-//
-// socket.on("message", writeEvent);
-// socket.on("newPlayer", writePlayers);
-
-// document
-//   .querySelector("#chat-form")
-//   .addEventListener("submit", onFormSubmitted);
-
-// document
-//   .querySelector("#nameForm")
-//   .addEventListener("submit", onFormSubmittedName);
+socket.on("askName", askName)
